a17868b73f53b24ab01f690e10bc0fb369cfdee9
leamscesar@gmail.com <leamscesar@gmail.com>
2019-04-18 17:20:57 -0300

    Displaying screen for shapes. Still not working

--- src/gui/OptionsControler.java
+++ src/gui/OptionsControler.java
@@ -3,30 +3,142 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import beans.Array;
+import beans.Shape;
+import beans.ShapeReader;
+import beans.Linear.Projections;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
-import javafx.scene.control.SplitMenuButton;
-
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class OptionsControler implements Initializable{
 	
-	private String objects [] = {"calice2.byu",
+	String objects [] = {"calice2.byu",
 			"maca.byu",
 			"maca2.byu",
 			"vaso.byu",
 			"vaca.byu",
 			"piramide.byu",
 			"triangulo.byu"	};
-	public String absolutePath = "/home/ismael/Documentos/Github/computacao_grafica/src/gui/fxml/";
+	String absolutePath = "/home/ismael/Documentos/Github/computacao_grafica/src/gui/fxml/";
+	//screen width and height
+	int width =512;
+	int height =512;
+	
+	ShapeReader sr = new ShapeReader();
+	String selectedShape= objects[0];
+	String tempSelected = objects[0];
 	
 	//Variables from FXML
 	@FXML
-	SplitMenuButton shapeSelectior;
+	MenuButton shapeSelector;
+	
+	@FXML
+	Pane shapeDisplayer;
+	
+	Canvas canvas;
+	
+	GraphicsContext gc;
+	PixelWriter pw ;
 	
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
+		initializeMenuButton();		
+		this.canvas = new Canvas(this.shapeDisplayer.getWidth(),this.shapeDisplayer.getHeight());
+		shapeDisplayer.getChildren().add(canvas);
+		gc = canvas.getGraphicsContext2D();
+		pw = gc.getPixelWriter(); 
+		drawDefaultShape();
+	}
+	
+	public void drawDefaultShape() {
+		Shape s = sr.read(objects[0]);
+		compute_coordinates_and_draw_pixels(s,this.pw);
+	}
+	
+	private void initializeMenuButton() {
+		int itemsN = 4;
+		MenuItem mItems[] = new MenuItem[itemsN];
+			
+		for(int  i = 0; i < itemsN; i++) {
+			mItems[0] = new MenuItem(this.objects[0]);
+			mItems[0].setOnAction(event ->{
+				selectShapeCallback(((MenuItem)event.getSource()).getText());
+				});
+		}
+		
+		this.shapeSelector = new MenuButton("Shapes");
+		for(int i =0; i < itemsN;i++)
+		   this.shapeSelector.getItems().add(mItems[i]);
+		this.shapeSelector.show();
+	}
+	
+	public void selectShapeCallback(String shapeSelected) {
+		shapeDisplayer.setStyle("-fx-background-color:black");
+		Shape s = this.sr.read(shapeSelected);
+		this.compute_coordinates_and_draw_pixels(s, this.pw);
+	}
+	
+	//Internal functions
+	public static double getDimentionMaxValue(Array vertexes[],int dim) {
+		double value= 0;
+			
+		for(int v = 0; v < vertexes.length;v++) {
+			if(vertexes[v].getItem(0,dim) > value)
+				value = vertexes[v].getItem(0,dim);
+		}
+		
+		return value;		
+	}
+	
+	public static double getDimentionMinValue(Array vertexes[],int dim) {
+		double value= 0;
+		value = vertexes[0].getItem(0,dim);
+		
+		for(int v = 0; v < vertexes.length;v++) {
+			if(vertexes[v].getItem(0,dim) < value)
+				value = vertexes[v].getItem(0,dim);
+		}		
+		return value;		
+	}
+	
+	public static Array[] applyOrthogonalProjectionToVertexSet(Shape s) {
+		Array values [] = new Array[s.getVertexes().length];		
+		for(int i = 0; i<s.getVertexes().length;i++) {
+			values[i] = Projections.orthogonal(s.getVertex(i));
+		}		
+		return values;	
+	}
+	
+	@SuppressWarnings("exports")
+	public void compute_coordinates_and_draw_pixels(Shape s,PixelWriter pw) {
+		Array shapeVertices [] = applyOrthogonalProjectionToVertexSet(s);
+		double x_max = getDimentionMaxValue(shapeVertices,0);
+		double x_min = getDimentionMinValue(shapeVertices,0);
+		
+		double y_max = getDimentionMaxValue(shapeVertices,1);
+		double y_min = getDimentionMinValue(shapeVertices,1);
+		Array v = null;
+		for(int i = 0; i < shapeVertices.length;i++) {
+			
+			//normalization
+			double x = shapeVertices[i].getItem(0,0);
+			double y = shapeVertices[i].getItem(0,1);
+			
+			x = (x-x_min)/(x_max-x_min)*(width-1);
+			y = (y-y_min)/(y_max-y_min)*(height-1);
+			//System.out.printf("%d,%d\n",x,y);
+			pw.setColor((int)x,(int)y,Color.WHITE);
+			
+		}
 		
 	}
 	
--- src/gui/Screen.java
+++ src/gui/Screen.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.net.URL;
+
 import beans.Array;
 import beans.Linear;
 import beans.Linear.Projections;
@@ -7,103 +9,35 @@ import beans.ShapeReader;
 import beans.Shape;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.PixelWriter;
-import javafx.scene.layout.Pane;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
+
 
 public class Screen extends Application{
-
-	private String objects [] = {"calice2.byu",
-			"maca.byu",
-			"maca2.byu",
-			"vaso.byu",
-			"vaca.byu",
-			"piramide.byu",
-			"triangulo.byu"	};
-	String absolutePath = "/home/ismael/Documentos/Github/computacao_grafica/src/gui/fxml/";
 	
-	int width =512;
-	int height =512;
-	Canvas canvas = new Canvas(width, height);
-	Pane p = new Pane(canvas);
-	Scene mScene = new Scene(p);
-	
-	
-	public static double getDimentionMaxValue(Array vertexes[],int dim) {
-		double value= 0;
-			
-		for(int v = 0; v < vertexes.length;v++) {
-			if(vertexes[v].getItem(0,dim) > value)
-				value = vertexes[v].getItem(0,dim);
-		}
-		
-		return value;		
-	}
-	
-	public static double getDimentionMinValue(Array vertexes[],int dim) {
-		double value= 0;
-		value = vertexes[0].getItem(0,dim);
-		
-		for(int v = 0; v < vertexes.length;v++) {
-			if(vertexes[v].getItem(0,dim) < value)
-				value = vertexes[v].getItem(0,dim);
-		}		
-		return value;		
-	}
-	
-	public static Array[] applyOrthogonalProjectionToVertexSet(Shape s) {
-		Array values [] = new Array[s.getVertexes().length];		
-		for(int i = 0; i<s.getVertexes().length;i++) {
-			values[i] = Projections.orthogonal(s.getVertex(i));
-		}		
-		return values;	
-	}
-	
-	@SuppressWarnings("exports")
-	public void compute_coordinates_and_draw_pixels(Shape s,PixelWriter pw) {
-		Array shapeVertices [] = applyOrthogonalProjectionToVertexSet(s);
-		double x_max = getDimentionMaxValue(shapeVertices,0);
-		double x_min = getDimentionMinValue(shapeVertices,0);
-		
-		double y_max = getDimentionMaxValue(shapeVertices,1);
-		double y_min = getDimentionMinValue(shapeVertices,1);
-		Array v = null;
-		for(int i = 0; i < shapeVertices.length;i++) {
-			
-			//normalization
-			double x = shapeVertices[i].getItem(0,0);
-			double y = shapeVertices[i].getItem(0,1);
-			
-			x = (x-x_min)/(x_max-x_min)*(width-1);
-			y = (y-y_min)/(y_max-y_min)*(height-1);
-			//System.out.printf("%d,%d\n",x,y);
-			pw.setColor((int)x,(int)y,Color.WHITE);
-			
-		}
-		
-	}
+	String absolutePath = "/home/ismael/Documentos/Github/computacao_grafica/src/gui/";
 	
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
-		p.setStyle("-fx-background-color:black");
-		
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		PixelWriter pw = gc.getPixelWriter();
-		
-		
-		ShapeReader sr = new ShapeReader();
-		Shape s = sr.read(objects[0]);
-		compute_coordinates_and_draw_pixels(s,pw);
-		
-		stage.setScene(mScene);
-		stage.setTitle("Teste");
-		stage.show();
+		try {
+			  String location = absolutePath+"OptionsControler.fxml";
+			  
+			  URL url = getClass().getResource("OptionsControler.fxml");
+			  Parent root = FXMLLoader.load(url);
+			  
+		      Scene scene = new Scene(root);
+		      primaryStage.setScene(scene);
+		      primaryStage.setTitle("Shape Displayer"); 
+		      primaryStage.show();
+		    } catch (Exception e) {
+		      System.out.println("Erro");
+		      System.out.println(e.getMessage());
+		      e.printStackTrace();
+		}
 	}
 	
 	public static void main(String []args) {
