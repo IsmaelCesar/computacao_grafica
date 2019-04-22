92898bd5e4e5d5ffdbb69b3be433965f8be7c6f8
leamscesar@gmail.com <leamscesar@gmail.com>
2019-04-22 14:09:02 -0300

    Fixing the selection items

--- src/gui/OptionsControler.java
+++ src/gui/OptionsControler.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import beans.Array;
@@ -28,8 +29,8 @@ public class OptionsControler implements Initializable{
 			"triangulo.byu"	};
 	String absolutePath = "/home/ismael/Documentos/Github/computacao_grafica/src/gui/fxml/";
 	//screen width and height
-	int width =512;
-	int height =512;
+	double width;
+	double height;
 	
 	ShapeReader sr = new ShapeReader();
 	String selectedShape= objects[0];
@@ -40,51 +41,48 @@ public class OptionsControler implements Initializable{
 	MenuButton shapeSelector;
 	
 	@FXML
-	Pane shapeDisplayer;
-	
 	Canvas canvas;
 	
 	GraphicsContext gc;
-	PixelWriter pw ;
-	
+		
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub		
 		initializeMenuButton();		
-		this.canvas = new Canvas(this.shapeDisplayer.getWidth(),this.shapeDisplayer.getHeight());
-		shapeDisplayer.getChildren().add(canvas);
-		gc = canvas.getGraphicsContext2D();
-		pw = gc.getPixelWriter(); 
+		gc = canvas.getGraphicsContext2D();			
+		this.width = canvas.getWidth();
+		this.height= canvas.getHeight();	
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, width, height);
 		drawDefaultShape();
 	}
 	
 	public void drawDefaultShape() {
 		Shape s = sr.read(objects[0]);
-		compute_coordinates_and_draw_pixels(s,this.pw);
+		compute_coordinates_and_draw_pixels(s,gc);
 	}
 	
 	private void initializeMenuButton() {
 		int itemsN = 4;
-		MenuItem mItems[] = new MenuItem[itemsN];
+		ArrayList<MenuItem> mItems = new ArrayList<MenuItem>();
 			
 		for(int  i = 0; i < itemsN; i++) {
-			mItems[0] = new MenuItem(this.objects[0]);
-			mItems[0].setOnAction(event ->{
+			mItems.add(new MenuItem(this.objects[i]));
+			mItems.get(i).setOnAction(event ->{
 				selectShapeCallback(((MenuItem)event.getSource()).getText());
 				});
 		}
 		
-		this.shapeSelector = new MenuButton("Shapes");
-		for(int i =0; i < itemsN;i++)
-		   this.shapeSelector.getItems().add(mItems[i]);
+		this.shapeSelector.getItems().addAll(mItems);
 		this.shapeSelector.show();
 	}
 	
 	public void selectShapeCallback(String shapeSelected) {
-		shapeDisplayer.setStyle("-fx-background-color:black");
-		Shape s = this.sr.read(shapeSelected);
-		this.compute_coordinates_and_draw_pixels(s, this.pw);
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0,0, this.width,this.height);
+		Shape s = sr.read(shapeSelected);
+		compute_coordinates_and_draw_pixels(s,gc);
 	}
 	
 	//Internal functions
@@ -119,7 +117,7 @@ public class OptionsControler implements Initializable{
 	}
 	
 	@SuppressWarnings("exports")
-	public void compute_coordinates_and_draw_pixels(Shape s,PixelWriter pw) {
+	public void compute_coordinates_and_draw_pixels(Shape s,GraphicsContext gc) {
 		Array shapeVertices [] = applyOrthogonalProjectionToVertexSet(s);
 		double x_max = getDimentionMaxValue(shapeVertices,0);
 		double x_min = getDimentionMinValue(shapeVertices,0);
@@ -136,7 +134,8 @@ public class OptionsControler implements Initializable{
 			x = (x-x_min)/(x_max-x_min)*(width-1);
 			y = (y-y_min)/(y_max-y_min)*(height-1);
 			//System.out.printf("%d,%d\n",x,y);
-			pw.setColor((int)x,(int)y,Color.WHITE);
+			gc.setFill(Color.WHITE);
+			gc.fillRect(x,y, 1,1);
 			
 		}
 		
